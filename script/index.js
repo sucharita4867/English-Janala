893b6a971a8883c80949cf620cea35ca8661bfdd
sucharita4867 <sucharitasardar7@gmail.com>
2025-09-05 20:30:14 +0530

    added spinner in the project

--- script/index.js
+++ script/index.js
@@ -1,3 +1,18 @@
+const createElement = (arr) => {
+  const htmlElements = arr.map((el) => `<span class ="btn">${el}</span>`);
+  return htmlElements.join(" ");
+};
+
+const manageSpinner = (status) => {
+  if (status == true) {
+    document.getElementById("spinner").classList.remove("hidden");
+    document.getElementById("word-container").classList.add("hidden");
+  } else {
+    document.getElementById("word-container").classList.remove("hidden");
+    document.getElementById("spinner").classList.add("hidden");
+  }
+};
+
 const loadLessons = () => {
   fetch("https://openapi.programming-hero.com/api/levels/all")
     .then((res) => res.json())
@@ -10,6 +25,7 @@ const removeActive = () => {
   lessonBtn.forEach((btn) => btn.classList.remove("active"));
 };
 const loadLevelWord = (id) => {
+  manageSpinner(true);
   // console.log(id);
   const url = `https://openapi.programming-hero.com/api/level/${id}`;
   // console.log(url);
@@ -22,6 +38,44 @@ const loadLevelWord = (id) => {
       clickBtn.classList.add("active");
       displayLevelWord(data.data);
     });
+};
+
+const loadWordDetail = async (id) => {
+  const url = `https://openapi.programming-hero.com/api/word/${id}`;
+  // console.log(url);
+  const res = await fetch(url);
+  const details = await res.json();
+  displayWordDetails(details.data);
+};
+
+const displayWordDetails = (word) => {
+  // console.log(word);
+  const detailsBox = document.getElementById("details-container");
+  detailsBox.innerHTML = `
+  
+  <div class="">
+                                    <h2 class="text-2xl font-bold">${
+                                      word.word
+                                    } ( <i class="fa-solid fa-microphone-lines"></i>
+                                          :${word.pronunciation})</h2>
+                              </div>
+                              <div class="">
+                                    <h2 class=" font-bold">Meaning</h2>
+                                    <p>${word.meaning}</p>
+                              </div>
+                              <div class="">
+                                    <h2 class=" font-bold">Example</h2>
+                                    <p>${word.sentence}</p>
+                              </div>
+                              <div class="">
+                                    <h2 class=" font-bold">Synonym</h2>
+                                    <div class="">${createElement(
+                                      word.synonyms
+                                    )}</div>
+                              </div>
+  
+  `;
+  document.getElementById("word-modal").showModal();
 };
 
 const displayLevelWord = (words) => {
@@ -38,6 +92,7 @@ const displayLevelWord = (words) => {
                         <h1 class="text-4xl font-bold">নেক্সট Lesson এ যান</h1>
                   </div>
     `;
+    manageSpinner(false);
     return;
   }
   words.forEach((word) => {
@@ -58,7 +113,9 @@ const displayLevelWord = (words) => {
                              : "pronunciation পাওয়া যায়নি"
                          }"</div>
                         <div class="flex justify-between items-center">
-                              <button onclick="my_modal_5.showModal()" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i
+                              <button onclick="loadWordDetail(${
+                                word.id
+                              })" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i
                                           class="fa-solid fa-circle-info"></i></button>
                               <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i
                                           class="fa-solid fa-volume-high"></i></button>
@@ -67,6 +124,7 @@ const displayLevelWord = (words) => {
     `;
     wordContainer.append(card);
   });
+  manageSpinner(false);
 };
 const displayLesson = (lessons) => {
   // 1 get the container & empty

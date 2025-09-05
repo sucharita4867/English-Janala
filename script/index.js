87bb531442186e265b5698035a8d37fa09d3ed21
sucharita4867 <sucharitasardar7@gmail.com>
2025-09-05 08:24:14 +0530

    create card

--- script/index.js
+++ script/index.js
@@ -4,6 +4,28 @@ const loadLessons = () => {
     .then((json) => displayLesson(json.data));
 };
 
+const loadLevelWord = (id) => {
+  // console.log(id);
+  const url = `https://openapi.programming-hero.com/api/level/${id}`;
+  // console.log(url);
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayLevelWord(data.data));
+};
+
+const displayLevelWord = (words) => {
+  const wordContainer = document.getElementById("word-container");
+  wordContainer.innerHTML = "";
+
+  words.forEach((word) => {
+    console.log(word);
+    const card = document.createElement("div");
+    card.innerHTML = `
+    <p>cat</p>
+    `;
+    wordContainer.append(card);
+  });
+};
 const displayLesson = (lessons) => {
   // 1 get the container & empty
   const levelContainer = document.getElementById("level-container");
@@ -15,7 +37,7 @@ const displayLesson = (lessons) => {
 
     const buttonDiv = document.createElement("div");
     buttonDiv.innerHTML = `
-    <button class="btn btn-outline btn-primary" >
+    <button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary" >
     <i class="fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}
     </button>
     `;

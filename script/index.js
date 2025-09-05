2de14080b8e0e6dc05bedb06e0d9054d48c2c49d
sucharita4867 <sucharitasardar7@gmail.com>
2025-09-05 12:34:11 +0530

    added this project card sectence

--- script/index.js
+++ script/index.js
@@ -17,11 +17,42 @@ const displayLevelWord = (words) => {
   const wordContainer = document.getElementById("word-container");
   wordContainer.innerHTML = "";
 
+  if (words.length == 0) {
+    wordContainer.innerHTML = `
+    <div class="text-center col-span-full rounded-xl space-y-6font-bengali py-8">
+                        <figure class="flex justify-center">
+                              <img src="./assets/alert-error.png" alt="">
+                        </figure>
+                        <p class="text-xl font-medium text-gray-400 ">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+                        <h1 class="text-4xl font-bold">নেক্সট Lesson এ যান</h1>
+                  </div>
+    `;
+    return;
+  }
   words.forEach((word) => {
     console.log(word);
     const card = document.createElement("div");
     card.innerHTML = `
-    <p>cat</p>
+    <div class=" bg-white rounded-xl text-center shadow-sm py-10 px-5 space-y-4">
+                        <h1 class="text-2xl font-bold">${
+                          word.word ? word.word : "শব্দ পাওয়া যায়নি"
+                        }</h1>
+                        <p class="font-semibold">Meaning /Pronounciation</p>
+                        <div class="text-2xl font-medium font-bengali">"${
+                          word.meaning ? word.meaning : "অর্থ পাওয়া যায়নি"
+                        }
+                         / ${
+                           word.pronunciation
+                             ? word.pronunciation
+                             : "pronunciation পাওয়া যায়নি"
+                         }"</div>
+                        <div class="flex justify-between items-center">
+                              <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i
+                                          class="fa-solid fa-circle-info"></i></button>
+                              <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i
+                                          class="fa-solid fa-volume-high"></i></button>
+                        </div>
+                  </div>
     `;
     wordContainer.append(card);
   });
